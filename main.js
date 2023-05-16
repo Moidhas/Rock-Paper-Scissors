a187ef071c70596f07547634302691d7eed43313
Moidhas <moidk3026@gmail.com>
2023-05-16 14:56:07 -0400

    Finished CSS styling and game features

--- main.js
+++ main.js
@@ -26,11 +26,11 @@ function computeWon(cs, scissors, paper, rock) {
 }
 
 
-function playRound(playerSelection, computerSelection, results) {
+function playRound(playerSelection, computerSelection) {
     let ps = playerSelection.toUpperCase();
     let cs = computerSelection.toUpperCase();
     let retval;
-
+    let won = 0; 
     if (ps === "ROCK") {
        retval = computeWon(cs, true, false, undefined);
     } else if (ps === "PAPER") {
@@ -40,20 +40,46 @@ function playRound(playerSelection, computerSelection, results) {
     }
 
     if (retval === "You Lose!") {
-        retval = retval + " " + computerSelection + " beats " + playerSelection;  
+        retval = retval + " " + computerSelection + " beats " + playerSelection;
+        won = -1;
     } else if (retval === "You Won!") {
         retval = retval + " " + playerSelection + " beats " + computerSelection;  
+        won = 1;  
     } else {
         retval = retval + " " + playerSelection + " === " + computerSelection;
     }
 
     results.textContent = retval; 
+    return won; 
 }
 
+
+
+let comp_won = 0; 
+let player_won = 0; 
 const results = document.querySelector('.results');
+const score = document.querySelector('.score');
 const btns = document.querySelectorAll('button');
 btns.forEach(btn => btn.addEventListener('click', e => {
     let playerSelection = btn.getAttribute('class');
     let computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection, results);
+    let won = playRound(playerSelection, computerSelection, results);
+    if (won === -1) {
+        comp_won += 1; 
+    } else if (won === 1) {
+        player_won += 1; 
+    }
+    const playAgainMsg = " Press to Play again";
+
+    if (comp_won === 5) {
+        results.textContent = "Computer Won?!?" + playAgainMsg;
+        comp_won = 0;
+        player_won = 0; 
+    } else if (player_won === 5) {
+        results.textContent = "Player Won!!!" + playAgainMsg;
+        comp_won = 0;
+        player_won = 0; 
+    }
+    
+    score.textContent = `Score: ${player_won}`;
 }));
